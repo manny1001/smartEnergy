c44d11eae3ce2d0d3b70c22127a391b38c3fb60c
Emmanuel <Emmanuelmuya@gmail.com>
2022-08-16 18:56:11 +0200

    Auth Ui ALmost done

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import {ForgotpasswordComponent} from './forgotpassword/forgotpassword.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
@@ -13,6 +14,8 @@ const routes: Routes = [
   },
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'forgotPassword', component: ForgotpasswordComponent },
+
 
 ];
 
