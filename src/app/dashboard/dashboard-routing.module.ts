39dbd8a164d1383898beb112c187d0018ef67e59
Emmanuel <Emmanuelmuya@gmail.com>
2022-09-22 08:40:59 +0200

    Updated UI

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
-
+import { CreateProjectComponent } from './projects/projects/projects.component';
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
 
   {
-    path:'home',
+    path: 'home',
     component: DashboardComponent,
-  }
+  },
+  {
+    path: 'projects',
+    component: CreateProjectComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
---
+++ src/app/dashboard/projects/projects/projects.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-project',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css'],
+})
+export class CreateProjectComponent implements OnInit {
+  selectionMade: boolean = false;
+  selectionType: string = ' ';
+  constructor() {}
+
+  ngOnInit() {}
+  registerAs(type: string) {
+    console.log({ type });
+    this.selectionMade = true;
+
+    if (type === 'IPP') {
+      this.selectionType = 'IPP';
+    }
+    if (type === 'Sponsor') {
+      this.selectionType = 'Sponsor';
+    }
+  }
+}
